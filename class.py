52db118d0d33a8d95163b6343ee00649caa55343
ValeEloss <104370745+ValeEloss@users.noreply.github.com>
2022-06-27 08:52:40 -0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,8 @@
+nombre='valeria'
+print('me llamo',nombre)
+grado='9c'
+print('voy en', grado)
+colorfavorito='azul'
+print('mi color favorito es el', colorfavorito)
+comidafavorita='pasta'
+print('mi comida favorita es la', comidafavorita)
